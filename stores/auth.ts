972ac8411989bc9702e7535c8385d8503e0e68e3
Vincent Lalanne <v.lalanne@fugu.fr>
2023-01-25 04:14:32 +0100

    Enhance auth store

--- stores/auth.ts
+++ stores/auth.ts
@@ -29,7 +29,9 @@ export const useAuthStore = defineStore("auth", () => {
   const errorHandler = async (fn: AnyFn) => {
     try {
       isLoading.value = true;
-      return await fn();
+      const result = await fn();
+      if (result.error) throw result.error;
+      return result;
     } catch (error: any) {
       console.log(error);
       ElMessage.error({ message: error.error_description || error.message });
@@ -42,92 +44,72 @@ export const useAuthStore = defineStore("auth", () => {
    * Login with email and password
    */
   const login = async ({ email, password }: SignInParams) =>
-    await errorHandler(async () => {
-      const { data: user, error } = await $auth.auth.signInWithPassword({
-        email,
-        password,
-      });
-      if (error) throw error;
-      return user;
-    });
+    await errorHandler(
+      async () =>
+        await $auth.auth.signInWithPassword({
+          email,
+          password,
+        })
+    );
 
   /**
    * Login with email (magic)
    */
   const loginWithEmail = async ({ email }: Pick<SignInParams, "email">) =>
-    await errorHandler(async () => {
-      const { data: user, error } = await $auth.auth.signInWithOtp({ email });
-      if (error) throw error;
-      return user;
-    });
+    await errorHandler(async () => await $auth.auth.signInWithOtp({ email }));
 
   /**
    * Login with google, github, etc
    */
 
   const loginWithSocialProvider = async (provider: Provider) =>
-    await errorHandler(async () => {
-      const { data: user, error } = await $auth.auth.signInWithOAuth({
-        provider,
-      });
-      if (error) throw error;
-      return user;
-    });
+    await errorHandler(
+      async () =>
+        await $auth.auth.signInWithOAuth({
+          provider,
+        })
+    );
 
   /**
    * Logout
    */
   const logout = async () =>
-    await errorHandler(async () => {
-      const { error } = await $auth.auth.signOut();
-      if (error) throw error;
-    });
+    await errorHandler(async () => await $auth.auth.signOut());
 
   /**
    * Register
    */
   const register = async ({ email, password, ...meta }: SignUpParams) =>
-    await errorHandler(async () => {
-      const { data: user, error } = await $auth.auth.signUp({
-        email,
-        password,
-      });
-      if (error) throw error;
-      return user;
-    });
+    await errorHandler(
+      async () =>
+        await $auth.auth.signUp({
+          email,
+          password,
+        })
+    );
 
   /**
    * Update user email, password, or meta data
    */
-  const update = async (data: Obj) =>
-    await errorHandler(async () => {
-      const { data: user, error } = await $auth.auth.updateUser(data);
-      if (error) throw error;
-      return user;
-    });
+  const update = async (userData: Obj) =>
+    await errorHandler(async () => await $auth.auth.updateUser(userData));
 
   /**
    * Send user an email to reset their password
    * (ie. support "Forgot Password?")
    */
   const sendPasswordRestEmail = async (email: string) =>
-    await errorHandler(async () => {
-      const { data, error } = await $auth.auth.resetPasswordForEmail(email);
-      if (error) throw error;
-      return data;
-    });
+    await errorHandler(
+      async () => await $auth.auth.resetPasswordForEmail(email)
+    );
 
   const resetPassword = async (accessToken: string, newPassword: string) =>
-    await errorHandler(async () => {
-      const { data: user, error } = await $auth.auth.admin.updateUserById(
-        accessToken,
-        {
+    await errorHandler(
+      async () =>
+        await $auth.auth.admin.updateUserById(accessToken, {
           password: newPassword,
-        }
-      );
-      if (error) throw error;
-      return user;
-    });
+        })
+    );
 
   return {
     user,

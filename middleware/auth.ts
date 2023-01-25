7543031000e84ff3eb7cd891930079c61f08894d
Vincent Lalanne <v.lalanne@fugu.fr>
2023-01-25 20:38:33 +0100

    Fix try 5

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -4,6 +4,13 @@ export default defineNuxtRouteMiddleware((to, from) => {
   const allowedPaths = ["login", "set-password", "reset-password"];
   const { isLoggedIn } = storeToRefs(useAuthStore());
 
+  console.log("middleware isLoggedIn", isLoggedIn.value);
+  console.log(
+    "middleware allowed path",
+    to.name,
+    allowedPaths.includes(String(to.name))
+  );
+
   if (!isLoggedIn.value && !allowedPaths.includes(String(to.name))) {
     return navigateTo("/login");
   }

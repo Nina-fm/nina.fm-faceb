6641f02dbfc268c7bad134ec081caf7b858bf37d
Vincent Lalanne <v.lalanne@fugu.fr>
2023-03-29 14:33:27 +0200

    fix: pageHeader cols

--- components/ui/PageHeader.vue
+++ components/ui/PageHeader.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-defineProps<{
+const props = defineProps<{
   title?: string
   actions?: {
     tooltip?: string
@@ -13,7 +13,37 @@ defineEmits<{
   (e: "back"): void
 }>()
 
+const slots = useSlots()
+const { actions } = toRefs(props)
 const hasBack = ref(false)
+const titleCols = computed(() => {
+  let xs = 12
+  let sm = 12
+
+  if (actions?.value?.length && slots.content) {
+    xs = 6
+    sm = 4
+  } else if (actions?.value?.length && !slots.content) {
+    xs = 6
+    sm = 6
+  } else if (!actions?.value?.length && slots.content) {
+    xs = 12
+    sm = 6
+  }
+
+  return {
+    xs: hasBack.value ? xs - 1 : xs,
+    sm: hasBack.value ? sm - 1 : sm,
+  }
+})
+const contentCols = computed(() => ({
+  xs: 12,
+  sm: actions?.value?.length ? 4 : 6,
+}))
+const actionsCols = computed(() => ({
+  xs: 6,
+  sm: slots.content ? 4 : 6,
+}))
 
 onMounted(() => {
   const instance = getCurrentInstance() // only available inside lifecycle hooks
@@ -27,15 +57,27 @@ onMounted(() => {
       <v-col v-if="hasBack" cols="1" align-self="center">
         <v-btn variant="text" icon="mdi-arrow-left" size="small" @click="$emit('back')" />
       </v-col>
-      <v-col align-self="center">
+      <v-col :cols="titleCols.xs" :sm="titleCols.sm" align-self="center">
         <slot name="title">
           <span class="text-h5 text-primary">{{ title }}</span>
         </slot>
       </v-col>
-      <v-col align-self="center">
+      <v-col
+        v-if="$slots.content"
+        :cols="contentCols.xs"
+        :sm="contentCols.sm"
+        :class="`order-last order-sm-${hasBack ? 3 : 2}`"
+        align-self="center"
+      >
         <slot name="content" />
       </v-col>
-      <v-col class="extra" align-self="center">
+      <v-col
+        v-if="actions?.length"
+        :cols="actionsCols.xs"
+        :sm="actionsCols.sm"
+        :class="`extra order-${hasBack ? 3 : 2} order-sm-last`"
+        align-self="center"
+      >
         <slot name="extra">
           <template v-for="(action, index) in actions" :key="index">
             <v-tooltip :text="action.tooltip" :disabled="!action.tooltip" location="bottom">

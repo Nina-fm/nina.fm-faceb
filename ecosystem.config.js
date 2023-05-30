e28b6f86ed7753a810263f69d3adb0428d9749f9
Vincent Lalanne <v.lalanne@fugu.fr>
2023-05-31 00:57:27 +0200

    fix: remove deploy pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,16 +8,4 @@ module.exports = {
       script: "./.output/server/index.mjs",
     },
   ],
-  deploy: {
-    // "production" is the environment name
-    production: {
-      user: "vince",
-      host: ["flux.nina.fm"],
-      ref: "origin/dev",
-      repo: "git@github.com:Nina-fm/nina.fm-faceb.git",
-      ssh_options: ["ForwardAgent=yes"],
-      path: "/var/www/html/nina.fm-faceb",
-      "post-deploy": "yarn && yarn build && pm2 startOrRestart ecosystem.config.js --env production",
-    },
-  },
 }
